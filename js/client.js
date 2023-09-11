1c1a8d9e2c567a637b25f5a28f0a81ebf984bdc2
Nepal Singh <nepalsss007@gmail.com>
2023-09-11 21:48:29 +0530

    updated client.js
    hello guys my name is nepal singh

--- js/client.js
+++ js/client.js
@@ -1,4 +1,5 @@
 const socket = io('http://localhost:8000');
+console.log("Hello");
 
 const form = document.getElementById("send-container");
 const messageInput = document.getElementById("messageInp");
